5c24c9a7c36a070e757b52d938b61fd0c152d7e3
raviprajapati01 <145858419+raviprajapati01@users.noreply.github.com>
2024-10-07 00:42:45 +0530

    Update projectsData.js

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,30 +1,24 @@
 export const projectsData = [
     {
+        projectName: 'StudyNotion-application',
+        projectDescription: 'StudyNotion is a fully functional ed-tech platform that enables users to create, consume, and rate educational content. The platform is built using the MERN stack, which includes ReactJS, NodeJS, MongoDB, and ExpressJS.',
+        imageUrl: 'https://github.com/user-attachments/assets/c93434ae-6de7-4d49-a8ea-ce4c290fb87f',
+        projectUrl: 'https://study-notion-frontend-gules.vercel.app/'
+    },
+
+    {
+        projectName: 'DreamHaven - application',
+        projectDescription: 'Dream Haven  is a full-stack real estate application that allows users to search, book, and manage property listings. The platform features user authentication, property booking with a calendar, wishlist functionality, and much more.',
+        imageUrl: 'https://github.com/user-attachments/assets/59c4df91-aaaf-456e-926b-a61eb336a9f9',
+        projectUrl: 'https://dream-haven-app.vercel.app/'
+    },
+
+    {
         projectName: 'My Portfolio!',
-        projectDescription:
-            ' Welcome to My Portfolio! Explore my creative journey and projects, from websites to designs. This platform showcases my passion for crafting experiences and pushing creative limits. Dive in, connect, and get inspired!',
+        projectDescription:' Welcome to My Portfolio! Explore my creative journey and projects, from websites to designs. This platform showcases my passion for crafting experiences and pushing creative limits. Dive in, connect, and get inspired!',
         imageUrl: 'https://github.com/Squidex/sdk-fern/assets/145858419/b774aae5-4bc7-4178-932f-a608935cd4ad',
-        videoUrl:
-            'https://www.youtube.com/playlist?list=PLGyA74h_S9NqJvvQ7-l9bUHHS6bMCkgo0',
-        projectUrl: 'http://theleanprogrammer.com/name-it/',
-    },
-    {
-        projectName: 'Weather App',
-        projectDescription:
-            'Stay informed with real-time updates on temperature, humidity, and forecasts for your location and beyond. Seamlessly designed for intuitive use, it is your go-to tool for weather updates on the go. You can explore and see magic',
-        imageUrl: 'https://github.com/Squidex/sdk-fern/assets/145858419/2033869a-5700-4d55-913f-a123423b20dd',
-        videoUrl:
-            'https://www.youtube.com/playlist?list=PLGyA74h_S9NppHNrzUSjMQbnuHS9jlAcY',
-        projectUrl: 'http://theleanprogrammer.com/flashtype/',
-    },
-    {
-        projectName: 'Shoping Cart!',
-        projectDescription:
-            'Seamlessly manage your shopping experience with intuitive features for browsing, adding items to your cart, and completing transactions. Elevate your online shopping journey with convenience and efficiency. ',
-        imageUrl: 'https://github.com/Squidex/sdk-fern/assets/145858419/122fa636-4f07-4d83-8565-b5882121c90c',
-        videoUrl:
-            'https://www.youtube.com/playlist?list=PLGyA74h_S9NrkKnaIrYeOolru4EakosHY',
-        projectUrl: 'http://theleanprogrammer.com/flex/',
+        videoUrl:'https://www.youtube.com/playlist?list=PLGyA74h_S9NqJvvQ7-l9bUHHS6bMCkgo0',
+        projectUrl: 'https://portfoliowebsite-mu-six.vercel.app/',
     },
     {
         projectName: 'Name It!',
@@ -33,7 +27,25 @@ export const projectsData = [
         imageUrl: 'https://github.com/twinklegenon/Speech/assets/145858419/4966a196-7773-481b-98d3-172e74b68f64',
         videoUrl:
             'https://www.youtube.com/playlist?list=PLGyA74h_S9NrjM7mUiSmpKfUntwEcgMB3',
-        projectUrl: 'http://theleanprogrammer.com/pokedex/',
+        projectUrl: 'https://raviprajapati01.github.io/nameit',
+    },
+    {
+        projectName: 'Weather App',
+        projectDescription:
+            'Stay informed with real-time updates on temperature, humidity, and forecasts for your location and beyond. Seamlessly designed for intuitive use, it is your go-to tool for weather updates on the go. You can explore and see magic',
+        imageUrl: 'https://github.com/Squidex/sdk-fern/assets/145858419/2033869a-5700-4d55-913f-a123423b20dd',
+        videoUrl:
+            'https://www.youtube.com/playlist?list=PLGyA74h_S9NppHNrzUSjMQbnuHS9jlAcY',
+        projectUrl: '',
+    },
+    {
+        projectName: 'Shoping Cart!',
+        projectDescription:
+            'Seamlessly manage your shopping experience with intuitive features for browsing, adding items to your cart, and completing transactions. Elevate your online shopping journey with convenience and efficiency. ',
+        imageUrl: 'https://github.com/Squidex/sdk-fern/assets/145858419/122fa636-4f07-4d83-8565-b5882121c90c',
+        videoUrl:
+            'https://www.youtube.com/playlist?list=PLGyA74h_S9NrkKnaIrYeOolru4EakosHY',
+        projectUrl: '',
     },
     {
         projectName: 'Password Generater',
@@ -41,6 +53,6 @@ export const projectsData = [
             'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
         imageUrl: 'https://github.com/Squidex/sdk-fern/assets/145858419/cc5e383c-0025-4bb3-bb02-b36cb1df3ea9',
         videoUrl: 'http://theleanprogrammer.com/aam',
-        projectUrl: 'http://madhavbahl.tech/portfolio/',
+        projectUrl: '',
     },
 ];
